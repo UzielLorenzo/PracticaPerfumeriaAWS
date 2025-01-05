aa67b542c5dc3700baaf351851dcaf2cfedc06be
UzielLorenzo <Uzielhidaigl@gmail.com>
2025-01-05 03:59:09 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,18 @@
 <?php
+// db.php
 // Archivo para conectar con la base de datos PostgreSQL
 $host = 'perfumeria-db.c9cqgowoy3k1.us-east-2.rds.amazonaws.com';
-$dbname = 'perfumeria_db';
-$username = 'AdminPerfumeria';
-$password = 'MiContrasenaSegura2025';
+$db = 'perfumeria_db';
+$user = 'AdminPerfumeria';
+$pass = 'MiContrasenaSegura2025';
+$port = '5432';
 
 try {
-    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conexión exitosa.";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
+    die();
 }
 ?>
